43deb278f4d422392329e0a1d4e9a61a3eb5883a
Precious Akams <preciousakams@yahoo.com>
2021-02-06 17:44:04 +0100

    graghqlLaravelApi

---
+++ index.js
@@ -0,0 +1,19 @@
+const { GraphQLServer } = require('graphql-yoga');
+const fetch = require('node-fetch');
+
+const baseURL = 'https://api.thecatapi.com'
+
+const resolvers = {
+    Query: {
+        breeds: () => {
+            return fetch(`${baseURL}/v1/breeds`).then(res =>res.json())
+        }
+    },
+}
+
+const server = new GraphQLServer({
+    typeDefs: './schema.graphql',
+    resolvers
+});
+
+server.start(() =>console.log('Server is running on http://localhost:4000') )
